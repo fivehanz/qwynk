9d4582676df20b7925964e31ea76c6c2d024ea5f
Hanz <haniel56@zoho.eu>
2024-11-09 13:19:52 +0530

    chore: rm elixir

--- lib/qwynk/repo.ex
+++
@@ -1,9 +0,0 @@
-defmodule Qwynk.Repo do
-  use AshPostgres.Repo,
-    otp_app: :qwynk
-  
-  # Installs extensions that ash commonly uses
-  def installed_extensions do
-    ["ash-functions", "uuid-ossp", "citext"]
-  end
-end
